f63eb2f8a9a7476e6a75e38c6b1195570eb45a92
dadak797 <dadak797@gmail.com>
2025-03-04 00:48:09 +0900

    Multi-threading test with std::thread

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,25 @@
 #include "logger_config.h"
-#include <pthread.h>
+#include <thread>
+#include <vector>
 
 #define NUM_THREADS 4
 
 
-void* print_hello(void* arg) {
-    int thread_id = *(int*)arg;
+void print_hello(int thread_id) {
     SPDLOG_INFO("Hello from thread {}", thread_id);
-    return nullptr;
 }
 
 int main() {
     app::init_logger();
 
-    pthread_t threads[NUM_THREADS];
-    int thread_ids[NUM_THREADS];
+    std::vector<std::thread> threads;
 
     for (int i = 0; i < NUM_THREADS; i++) {
-        thread_ids[i] = i;
-        pthread_create(&threads[i], nullptr, print_hello, &thread_ids[i]);
+        threads.emplace_back(print_hello, i);
     }
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], nullptr);
+    for (auto& th: threads) {
+        th.join();
     }
 
     app::shutdown_logger();
